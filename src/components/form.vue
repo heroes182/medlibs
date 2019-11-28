fa8752594ec9a93789f97f788167e8e776445ef9
Gustavo Herodier <gustavo@tauri-tec.com>
2019-11-28 13:54:35 +0000

    generalizing styles

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="fields">
+    <div class="fields flex">
         <div v-for="(field, index) in fields"
             :key="index"
             class="field">
             <label class="label"
                 v-text="field.label +': ' + field.value" />
-            <div class="options">
+            <div v-if="field.options"
+                 class="flex">
                 <div v-for="(option, index) in field.options"
                     :key="index"
                     :class="{'selected': option === field.value}"
                     class="option"
                     v-text="option"
                     @click="field.value = option" />
+            </div>
+            <div class="flex"
+                 v-else>
+                <input type="number"
+                       v-model="field.value" />
             </div>
         </div>
     </div>
